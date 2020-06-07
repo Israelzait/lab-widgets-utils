78fc6ea63960a4adaf6061bfd39e25cd977c1279
jamescardona11 <jjcardonao@gmail.com>
2020-06-07 02:51:13 -0500

    Create animation for a flipCard

--- lib/flipcard/flipcard.dart
+++ lib/flipcard/flipcard.dart
@@ -1,13 +1,102 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class BackgroundCard extends StatelessWidget {
+class FlipCardWidget extends StatefulWidget {
+  @override
+  _FlipCardWidgetState createState() => _FlipCardWidgetState();
+}
+
+class _FlipCardWidgetState extends State<FlipCardWidget> with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animationRotating;
+  Animation<double> animationOpacityOut;
+  Animation<double> animationOpacityIn;
+
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+
+    animationRotating = Tween(begin: 0.0, end: -1.0 * math.pi).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    animationOpacityOut = Tween(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.25, 0.5, curve: Curves.easeIn),
+      ),
+    );
+
+    animationOpacityIn = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.52, 1.0, curve: Curves.easeIn),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    animationController.forward();
+
+    return Container(
+      width: 500, // TODO responsive sizes FIX
+      height: 270,
+      child: AnimatedBuilder(
+          animation: animationController,
+          builder: (context, child) {
+            print('animationRotating ${animationRotating.status}');
+            print('animationRotating ${animationRotating.value}');
+
+            return Stack(
+              children: [
+                Transform(
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateY(animationRotating.value),
+                  alignment: FractionalOffset.center,
+                  child: _BackgroundCard(
+                    borderRadius: BorderRadius.circular(25),
+                    image: AssetImage('assets/bg4.jpeg'),
+                    child: Opacity(
+                      opacity: animationOpacityOut.value,
+                      child: Center(
+                        child: Text('FRONT FACE', style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ),
+                ),
+                Transform(
+                  transform: Matrix4.rotationY(-1.0 * math.pi)
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateY(-animationRotating.value),
+                  alignment: FractionalOffset.center,
+                  child: Opacity(
+                    opacity: animationOpacityIn.value,
+                    child: Center(child: Text('BACK FACE', style: TextStyle(color: Colors.white))),
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
+
+class _BackgroundCard extends StatelessWidget {
   final Widget child;
   final ImageProvider image;
   final BorderRadius borderRadius;
   final bool shadow;
   final Color backgroundColor;
 
-  const BackgroundCard({
+  const _BackgroundCard({
     Key key,
     @required this.child,
     this.image,
--- lib/pages/flipcard_page.dart
+++ lib/pages/flipcard_page.dart
@@ -6,19 +6,18 @@ class FlipCardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 500,
-      height: 270,
-      padding: EdgeInsets.all(25),
-      child: Stack(
-        children: [
-          BackgroundCard(
-            borderRadius: BorderRadius.circular(25),
-            child: Center(
-              child: Text('text', style: TextStyle()),
-            ),
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(15),
+          child: FlipCardWidget(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //setState(() {});
+          },
+          child: Icon(Icons.transform),
+        ),
       ),
     );
   }
